481e6ab27259beb3c196e2ca41b56e88a374bedd
johnshado <dystopicjohn@gmail.com>
2023-02-20 00:43:28 -0500

    shadowjohn first version

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     let hoy = new Date(Date.now());
-    res.render('index',{title:'Shadow John Web', year:hoy.getFullYear()});
+    res.render('index', { title: 'Shadow John Web', year: hoy.getFullYear() });
 })
 
 //export router
